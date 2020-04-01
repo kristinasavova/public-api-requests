de442305f0b93de58540a7d7c51ce6b891116a95
Kristina <ccthw@protonmail.com>
2020-04-01 20:06:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -14,14 +14,31 @@ function buildElement (elementType, elementClassName, elementParent) {
     return element; 
 } 
 
-const cardDiv = buildElement ('div', 'card', galleryDiv);
-const cardImageContainer = buildElement ('div', 'card-img-container', cardDiv); 
-const cardInfoContainer = buildElement ('div', 'card-info-container', cardDiv); 
-const cardImage = buildElement ('img', 'card-img', cardImageContainer); 
+/**
+ * A function that renders created HTML on the page
+ * @param {string} picture - profile picture
+ * @param {string} name - employee's first name  
+ * @param {string} surname - employee's last name  
+ * @param {string} email - employee's email 
+ * @param {string} city - employee's city 
+ * @param {string} state - employee's country 
+ */
+function renderHTMLCard (picture, name, surname, email, city, state) {
+    const cardDiv = buildElement ('div', 'card', galleryDiv); // create a card div
+    const cardImageContainer = buildElement ('div', 'card-img-container', cardDiv); // create image div  
+    const cardInfoContainer = buildElement ('div', 'card-info-container', cardDiv); // create info div 
+    const cardImage = buildElement ('img', 'card-img', cardImageContainer); // create image  
+    cardImage.setAttribute ('src', picture); 
+    cardImage.setAttribute ('alt', 'profile picture');
+    cardInfoContainer.innerHTML = `
+        <h3 id="name" class="card-name cap">${name} ${surname}</h3>
+        <p class="card-text">${email}</p>
+        <p class="card-text cap">${city}, ${state}</p>
+    `}
 
 /**
  * A function to fetch data 
- * @param {object} url - URL-address to fetch the data  
+ * @param {string} url - URL-address to fetch the data  
  */
 function fetchData (url) {
     return fetch (url)
@@ -43,8 +60,19 @@ function checkStatus (response) {
 }
 
 fetchData ('https://randomuser.me/api/?results=12')
-    .then (data => console.log (data));
-
+    .then (object => {
+        const profiles = object.results; // access the array of profiles 
+        console.log (profiles);
+        for (let i = 0; i < profiles.length; i ++) {
+            const profilePicture = profiles[i].picture.thumbnail; // access thumbnail pictures 
+            const profileName = profiles[i].name.first; // access first name
+            const profileLastName = profiles[i].name.last; // access last name 
+            const profileEmail = profiles[i].email; // access email 
+            const profileCity = profiles[i].location.city; // access city
+            const profileState = profiles[i].location.country; // access country 
+            renderHTMLCard (profilePicture, profileName, profileLastName, profileEmail, profileCity, profileState); 
+        }      
+    })
 
 
 
