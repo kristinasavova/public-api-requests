072f962cb05b72bbd6b29e48c33199ab8eed0aba
Kristina <ccthw@protonmail.com>
2020-04-02 18:38:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
 const galleryDiv = document.querySelector ('#gallery'); 
+const cardDiv = document.querySelectorAll ('.card'); 
 const documentBody = document.querySelector ('body');
 const script = document.querySelector ('script'); 
 
@@ -18,39 +19,41 @@ function buildElement (elementType, elementClassName, elementParent) {
 
 /**
  * A function that creates and renders cards of employees on the page
- * @param {string} picture - profile picture
- * @param {string} name - employee's first name  
- * @param {string} surname - employee's last name  
- * @param {string} email - employee's email 
- * @param {string} city - employee's city 
- * @param {string} state - employee's country 
  */
-function renderCard (picture, name, surname, email, city, state) {
+function buildHTML (user) {
     const cardDiv = buildElement ('div', 'card', galleryDiv); // create a card div
     const cardImageContainer = buildElement ('div', 'card-img-container', cardDiv); // create image div  
     const cardInfoContainer = buildElement ('div', 'card-info-container', cardDiv); // create info div 
     const cardImage = buildElement ('img', 'card-img', cardImageContainer); // create image  
-    cardImage.setAttribute ('src', picture); 
+    cardImage.setAttribute ('src', user.picture.large); 
     cardImage.setAttribute ('alt', 'profile picture');
     cardInfoContainer.innerHTML = `
-        <h3 id="name" class="card-name cap">${name} ${surname}</h3>
-        <p class="card-text">${email}</p>
-        <p class="card-text cap">${city}, ${state}</p>
-    `}
-
-function renderModal () {
-    const modalContainer = document.createElement ('div'); // create modal container 
-    modalContainer.className = 'modal-container';  
-    documentBody.insertBefore (modalContainer, script); // append modal container to body
-    const modalDiv = buildElement ('div', 'modal', modalContainer); // create modal div
-    const modalButton = buildElement ('button', 'modal-close-btn', modalDiv); // create modal button 
-    modalButton.setAttribute ('type', 'button'); 
-    modalButton.setAttribute ('id', 'modal-close-btn'); 
-    modalButton.innerHTML = '<strong>X</strong>'; 
-    const modalInfoContainer = buildElement ('div', 'modal-info-container', modalDiv); 
+        <h3 id="name" class="card-name cap">${user.name.first} ${user.name.last}</h3>
+        <p class="card-text">${user.email}</p>
+        <p class="card-text cap">${user.location.city}, ${user.location.country}</p>`
+    cardDiv.addEventListener ('click', () => { // show model container if card is clicked
+        const modalContainer = document.createElement ('div'); // create modal container 
+        modalContainer.className = 'modal-container';  
+        documentBody.insertBefore (modalContainer, script); // append modal container to body
+        const modalDiv = buildElement ('div', 'modal', modalContainer); // create modal div
+        const modalButton = buildElement ('button', 'modal-close-btn', modalDiv); // create modal button 
+        modalButton.setAttribute ('type', 'button'); 
+        modalButton.setAttribute ('id', 'modal-close-btn'); 
+        modalButton.innerHTML = '<strong>X</strong>'; 
+        modalButton.addEventListener ('click', () => { // hide modal container if close button is clicked
+            modalContainer.style.display = 'none'; 
+        });
+        const modalInfoContainer = buildElement ('div', 'modal-info-container', modalDiv); 
+        modalInfoContainer.innerHTML = `
+            <img class="modal-img" src=${user.picture.large} alt="profile picture">
+            <h3 id="name" class="modal-name cap">${user.name.first} ${user.name.last}</h3>
+            <p class="modal-text">${user.email}</p>
+            <p class="modal-text cap">${user.location.city}</p><hr>
+            <p class="modal-text">${user.cell}</p>
+            <p class="modal-text">${user.location.street.number} ${user.location.street.name}, ${user.location.state}, ${user.location.country} ${user.location.postcode}</p>
+            <p class="modal-text">Birthday: ${user.dob.date.slice (0, 10)}</p>
+    `});
 }
-
-renderModal ();
 
 /**
  * A function to fetch data 
@@ -80,20 +83,11 @@ fetchData ('https://randomuser.me/api/?results=12')
         const profiles = object.results; // access the array of profiles 
         console.log (profiles);
         for (let i = 0; i < profiles.length; i ++) {
-            const profilePicture = profiles[i].picture.thumbnail; // access thumbnail pictures 
-            const profileName = profiles[i].name.first; // access first name
-            const profileLastName = profiles[i].name.last; // access last name 
-            const profileEmail = profiles[i].email; // access email 
-            const profileCity = profiles[i].location.city; // access city
-            const profileState = profiles[i].location.country; // access country 
-            renderCard (profilePicture, profileName, profileLastName, profileEmail, profileCity, profileState); 
+            const employee = profiles[i]; 
+            buildHTML (employee);
         }      
     })
 
-document.querySelector ('#modal-close-btn').addEventListener ('click', () => { // close modal div 
-    const modalContainer = document.querySelector ('.modal-container'); 
-    modalContainer.style.display = 'none'; 
-})
 
 
 
